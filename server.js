d7b0f69e4c6f64f46b9fd5688d8ac5a1e6fd95ba
njohnbritto <nirmaljulietneena.johnbritto@charter.com>
2020-04-17 00:22:20 -0600

    for heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var htmlRoutes = require("./routes/html-routes");
 var apiRoutes = require("./routes/api-routes");
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
